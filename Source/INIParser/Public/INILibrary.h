64fae8a53263b42367670fd23d06daa745a4d14a
MrRobin <mrrobinjohannesson@gmail.com>
2023-01-20 22:55:25 +0100

    Init

--- Source/INIParser/Public/INILibrary.h
+++ Source/INIParser/Public/INILibrary.h
@@ -15,42 +15,42 @@ public:
 
 	UFUNCTION(
 		BlueprintCallable,
-		Category = "Libraries|INILibrary",
+		Category = "INIParser|INILibrary",
 		meta = (DisplayName = "Load .Ini File")
 	)
 	static TArray<FString> LoadINIFile(FString FilePath);
 
 	UFUNCTION(
 		BlueprintCallable,
-		Category = "Libraries|INILibrary",
+		Category = "INIParser|INILibrary",
 		meta = (DisplayName = "Try Load .Ini File")
 	)
 	static bool TryLoadINIFile(FString FilePath, TArray<FString>& OutContents);
 
 	UFUNCTION(
 		BlueprintCallable,
-		Category = "Libraries|INILibrary",
+		Category = "INIParser|INILibrary",
 		meta = (DisplayName = "Write .Ini File")
 	)
 	static void WriteINIFile(FString FilePath, TArray<FString> Contents);
 
 	UFUNCTION(
 		BlueprintCallable,
-		Category = "Libraries|INILibrary",
+		Category = "INIParser|INILibrary",
 		meta = (DisplayName = "Try Write .Ini File")
 	)
 	static bool TryWriteINIFile(FString FilePath, TArray<FString> Contents);
 
 	UFUNCTION(
 		BlueprintCallable,
-		Category = "Libraries|INILibrary",
+		Category = "INIParser|INILibrary",
 		meta = (DisplayName = "Parse .Ini File")
 	)
 	static TArray<FString> ParseINIFile(FString Data);
 
 	UFUNCTION(
 		BlueprintCallable,
-		Category = "Libraries|INILibrary",
+		Category = "INIParser|INILibrary",
 		meta = (DisplayName = "Try Parse .Ini File")
 	)
 	static bool TryParseINIFile(FString Data, TArray<FString>& OutContents);
